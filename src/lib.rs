fcebf43b5fbfd6e03276fddbbcb422b57ea8966c
HarryPan <1392882195@qq.com>
2023-03-25 16:41:54 +0800

    新增了对行数不匹配的处理

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,73 @@ pub mod lib {
         FormatError,
         LineError,
     }
+
+    pub fn open_file_from_argument(
+        arg: &Vec<String>,
+    ) -> (String, String, BufWriter<File>, BufWriter<File>) {
+        if arg.len() != 3 {
+            errordeal(Errinfo::ArgumentWrong, &Vec::<i32>::new());
+        }
+        //从参数数组提取读取汉化文件的名称并检验是否打开成功
+        let input_file_name = &arg[1];
+        let input = match fs::read_to_string(input_file_name) {
+            Ok(f) => f,
+            Err(_) => errordeal(Errinfo::FileOpen, &vec![input_file_name]),
+        };
+        //从参数数组提取读取日文文件的名称并检验是否打开成功
+        let japanese_file_name = &arg[2];
+        let inputJ = match fs::read_to_string(japanese_file_name) {
+            Ok(f) => f,
+            Err(_) => errordeal(Errinfo::FileOpen, &vec![japanese_file_name]),
+        };
+        //创建写入文件
+        let mut output_file_name = input_file_name.clone();
+        output_file_name.push_str(".tsv");
+        let output_file = match File::create(output_file_name.clone()) {
+            Ok(f) => f,
+            Err(_) => errordeal(Errinfo::FileCreate, &vec![output_file_name]),
+        };
+        let output = BufWriter::new(output_file);
+        //创建信息文件
+        let mut info_file_name = input_file_name.clone();
+        info_file_name.push_str("_info.txt");
+        let info_file = match File::create(info_file_name.clone()) {
+            Ok(f) => f,
+            Err(_) => errordeal(Errinfo::FileCreate, &vec![info_file_name]),
+        };
+        let mut info = BufWriter::new(info_file);
+        //写入抬头，方便查阅
+        info.write(b"JPN\t\tCHN\t\tLine\n");
+        info.flush();
+        return (input, inputJ, output, info);
+    }
+    //错误处理函数，根据Errinfo判断错误类型，并输出相应信息
+    pub fn errordeal<T: std::fmt::Display>(info: Errinfo, arg: &Vec<T>) -> ! {
+        match info {
+            Errinfo::ArgumentWrong => {
+                println!("Error:Wrong argument.");
+            }
+            Errinfo::FileOpen => {
+                println!("Error:Failed to open file:{}", arg[0]);
+            }
+            Errinfo::FileCreate => {
+                println!("Error:Failed to create file:{}", arg[0]);
+            }
+            Errinfo::FormatError => {
+                println!("Error:Unformated text.Line:{}", arg[0]);
+            }
+            Errinfo::LineError => {
+                println!(
+                    "Error:Unmatch lines.\n
+                            Size of Japanese buffer:{}\n
+                            Size of Chinese buffer:{}
+                            Line:{}",
+                    arg[0], arg[1], arg[2]
+                );
+            }
+        }
+        exit(0);
+    }
     pub fn standing(line: &str) -> Result<Signal, String> {
         //待机状态
         println!("{}", line);
@@ -66,88 +133,42 @@ pub mod lib {
         cbuff: &mut Iter<String>,
         input: &mut BufWriter<File>,
     ) -> Result<Signal, String> {
-        let mut buff = Vec::new();
-        if jbuff.len() == cbuff.len() {
-            let mut jline = String::new();
-            let mut cline = String::new();
-            loop {
-                jline = match jbuff.next() {
-                    Some(s) => s.to_string(),
-                    None => break,
-                };
-                cline = match cbuff.next() {
-                    Some(s) => s.to_string(),
-                    None => break,
-                };
-                input.write(b"on\t");
-                GBK.encode_to(&jline, EncoderTrap::Ignore, &mut buff);
-                input.write_all(&buff);
-                input.write(b"\t");
-                buff.clear();
-                GBK.encode_to(&cline, EncoderTrap::Ignore, &mut buff);
-                input.write_all(&buff);
-                input.write(b"\t\n");
-                buff.clear();
-                input.flush().unwrap();
-            }
-        } else {
-            return Err(String::from("error:unmatch lines."));
+        let mut buff = Vec::new(); //用于转码的缓冲区
+        let mut jline = String::new(); //当前写入的日文行
+        let mut cline = String::new(); //当前写入的译文行
+        loop {
+            jline = match jbuff.next() {
+                Some(s) => s.to_string(),
+                None => break,
+            };
+            cline = match cbuff.next() {
+                Some(s) => s.to_string(),
+                None => break,
+            };
+            //提示信息
+            println!("writing:\n{}\t\t{}", jline, cline);
+            input.write(b"on\t");
+            GBK.encode_to(&jline, EncoderTrap::Ignore, &mut buff);
+            input.write_all(&buff);
+            input.write(b"\t");
+            buff.clear();
+            GBK.encode_to(&cline, EncoderTrap::Ignore, &mut buff);
+            input.write_all(&buff);
+            input.write(b"\t\n");
+            buff.clear();
+            input.flush().unwrap();
         }
 
-        Ok(Signal::Standing)
+        Ok(Signal::Standing) //写入完毕后切换为待机状态
     }
-
-    pub fn open_file_from_argument(arg: &Vec<String>) -> (String, String, BufWriter<File>) {
-        if arg.len() != 3 {
-            errordeal(Errinfo::ArgumentWrong, &Vec::<i32>::new());
-        }
-        //从参数数组提取读取汉化文件的名称并检验是否打开成功
-        let input_file_name = &arg[1];
-        let input = match fs::read_to_string(input_file_name) {
-            Ok(f) => f,
-            Err(_) => errordeal(Errinfo::FileOpen, &vec![input_file_name]),
-        };
-        //从参数数组提取读取日文文件的名称并检验是否打开成功
-        let japanese_file_name = &arg[2];
-        let inputJ = match fs::read_to_string(japanese_file_name) {
-            Ok(f) => f,
-            Err(_) => errordeal(Errinfo::FileOpen, &vec![japanese_file_name]),
-        };
-        //创建写入文件
-        let mut output_file_name = input_file_name.clone();
-        output_file_name.push_str(".tsv");
-        let output_file = match File::create(output_file_name.clone()) {
-            Ok(f) => f,
-            Err(_) => errordeal(Errinfo::FileCreate, &vec![output_file_name]),
-        };
-        let output = BufWriter::new(output_file);
-        return (input, inputJ, output);
-    }
-
-    pub fn errordeal<T: std::fmt::Display>(info: Errinfo, arg: &Vec<T>) -> ! {
-        match info {
-            Errinfo::ArgumentWrong => {
-                println!("Error:Wrong argument.");
-            }
-            Errinfo::FileOpen => {
-                println!("Error:Failed to open file:{}", arg[0]);
-            }
-            Errinfo::FileCreate => {
-                println!("Error:Failed to create file:{}", arg[0]);
-            }
-            Errinfo::FormatError => {
-                println!("Error:Unformated text.Line:{}", arg[0]);
-            }
-            Errinfo::LineError => {
-                println!(
-                    "Error:Unmatch lines.\n
-                            Size of Japanese buffer:{}\n
-                            Size of Chinese buffer:{}
-                            Line:{}",
-                    arg[0], arg[1], arg[2]
-                );
-            }
-        }
-        exit(0);
+    //将行数信息写入译文文件
+    pub fn printinfo<T: std::fmt::Display>(arg: &Vec<T>, info: &mut BufWriter<File>) {
+        info.write(&arg[0].to_string().into_bytes());
+        info.write(b"\t\t");
+        info.write(&arg[1].to_string().into_bytes());
+        info.write(b"\t\t");
+        info.write(&arg[2].to_string().into_bytes());
+        info.write(b"\n");
+        info.flush();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,17 @@ use std::env;
 fn main() {
     //参数数组，检验参数是否合法
     let arg: Vec<String> = env::args().collect();
-    //创建读取汉化文件,读取日文文件和输出tsv文件的标头
-    let (input, inputJ, mut output) = open_file_from_argument(&arg);
+    //创建读取汉化文件,读取日文文件和输出tsv文件,差异行记录文件info的标头
+    let (input, inputJ, mut output, mut info) = open_file_from_argument(&arg);
     //创建双语缓冲jbuff,cbuff
     let mut jbuff: Vec<String> = Vec::new();
     let mut cbuff: Vec<String> = Vec::new();
     let mut a: i32 = 0; //行计数
     let mut sign = Signal::Standing; //状态标记
-                                     //    for line in input.lines() {
-    let mut line = String::new();
-    let mut lineJ = String::new();
-    let mut iter_input = input.lines();
-    let mut iter_inputJ = inputJ.lines();
+    let mut line = String::new(); //汉化的处理行
+    let mut lineJ = String::new(); //日文文件的处理行，根据分割线和原文对齐
+    let mut iter_input = input.lines(); //汉化的迭代器
+    let mut iter_inputJ = inputJ.lines(); //日文文件的迭代器
     loop {
         line = match iter_input.next() {
             Some(i) => i.to_string(),
@@ -35,34 +34,54 @@ fn main() {
             None => break,
         };
         a += 1;
+        //输出提示信息
         match sign {
             Signal::Standing => println!("{},Standing", a),
             Signal::Japanese => println!("{},Japanese", a),
             Signal::Chinese => println!("{},Chinese", a),
         }
+        //根据sign判断状态
         match sign {
             Signal::Standing => match standing(&line) {
                 Ok(s) => sign = s,
                 Err(_) => errordeal(Errinfo::FormatError, &vec![a]),
             },
+
             Signal::Japanese => sign = japanese(&line, &lineJ, &mut jbuff),
+
             Signal::Chinese => {
+                //如果返回false则进入writing步骤
                 if let Ok(false) = chinese(&line, &mut cbuff) {
-                    match writing(&mut jbuff.iter(), &mut cbuff.iter(), &mut output) {
-                        Ok(s) => {
-                            //写入成功就清空双语缓存并进入待机状态
-                            sign = s;
-                            jbuff.clear();
-                            cbuff.clear();
+                    //如果日语行比译文行多，则认为译文合并了行，将行数信息输出到info，并补全译文的行数
+                    if jbuff.len() > cbuff.len() {
+                        printinfo(
+                            &vec![
+                                jbuff.len().try_into().unwrap(),
+                                cbuff.len().try_into().unwrap(),
+                                a,
+                            ],
+                            &mut info,
+                        );
+                        while jbuff.len() != cbuff.len() {
+                            cbuff.push(String::from(""));
+                            iter_inputJ.next(); //补全时要让日文文件的迭代器前进，以保证与译文文件的对齐
                         }
-                        Err(_) => errordeal(
+                    } else if jbuff.len() < cbuff.len() {
+                        //如果日语行比译文行多，则认为译文拆分了行，进入错误处理步骤，需视情况自行修改译文文件
+                        errordeal(
                             Errinfo::LineError,
                             &vec![
                                 jbuff.len().try_into().unwrap(),
                                 cbuff.len().try_into().unwrap(),
                                 a,
                             ],
-                        ),
+                        )
+                    }
+                    if let Ok(s) = writing(&mut jbuff.iter(), &mut cbuff.iter(), &mut output) {
+                        //写入成功就清空双语缓存并进入待机状态
+                        sign = s;
+                        jbuff.clear();
+                        cbuff.clear();
                     }
                 }
             }
